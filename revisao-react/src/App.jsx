db677a0539423d9732a6e293897d8ec25e301b0c
arantesandre <arantes.andreluiz@gmail.com>
2025-02-05 20:58:48 -0300

    Add pasta revisao-react

---
+++ revisao-react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+// imports components
+import Header from './components/Header/Header';
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+
+}
+
+export default App;
---
+++ revisao-react/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+const Header = () => {
+
+    return (
+        <>
+            <div>
+                Header
+            </div>
+        </>
+    );
+}
+
+export default Header;
